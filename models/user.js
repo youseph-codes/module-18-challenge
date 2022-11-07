d2d83052130767b8a8378339afa48eb55fae013b
Yousopp <yousephagueye@gmail.com>
2022-11-07 16:43:06 -0500

    updated user.js model

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/.+\@.+\mm+/, 'Must match a legit email address.']
+        },
+        thoughts: []
+    }
+)
