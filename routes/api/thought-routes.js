4c81aa77ecf69ba0c983d7b265af77da181542f1
Yousopp <yousephagueye@gmail.com>
2022-11-12 01:00:23 -0500

    created router.routes in thoughts-routes.js

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -8,3 +8,10 @@ const {
     CREATEreaction,
     DELETEreaction,
 } = require('../../controllers/thought-controller');
+
+router.route('/').get(ALLthoughts).post(CREATEthought);
+router.route('/:id').get(getTHOUGHTbyID).put(UPDATEthought).delete(DELETEthought);
+router.route('/:thoughtID/reactions').post(CREATEreaction);
+router.route('/:thoughtID/reactions').delete(DELETEreaction);
+
+module.exports = router;
