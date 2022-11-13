1a12aea91d10cdb7b0aa4e08f69e80b285b46167
Yousopp <yousephagueye@gmail.com>
2022-11-12 21:51:03 -0500

    created schema

--- models/user.js
+++ models/user.js
@@ -1,3 +1,24 @@
 const { isEmail } = require("validator");
 const { Schema, model, Types } = require("mongoose");
 
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        true: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [isEmail, "Email is invalid!",],
+    },
+    thoughts: [
+        {
+            type:Schema.Types.ObjectId,
+            ref: "Thought",
+        },
+    ],
+    
+})
