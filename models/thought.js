779bb769416c9e47ff71bbffb0d8625442b7b385
Yousopp <yousephagueye@gmail.com>
2022-11-12 22:42:23 -0500

    created Schema for thought.js

--- models/thought.js
+++ models/thought.js
@@ -1,1 +1,20 @@
 const {Schema, model, Types } = require("mongoose");
+
+const thoughtSchema = new Schema({
+    thoughtTEXT: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+},
+{toJSON: {
+    virtuals: true,
+    getters: true,
+},
+id: false,
+});
